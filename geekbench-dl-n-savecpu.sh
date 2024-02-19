5abbe203ea6cc112b68ee281da783fd842664677
Nick Foster <me@nickfost.com>
2024-02-19 17:25:26 -0600

    Update geekbench-dl-n-savecpu.sh

--- geekbench-dl-n-savecpu.sh
+++ geekbench-dl-n-savecpu.sh
@@ -9,9 +9,8 @@ GBDIR=Geekbench-6.2.2-Linux
 wget https://cdn.geekbench.com/$GBVER
 tar -zxf $GBVER
 cd $GBDIR
-date > hardware.txt
-./geekbench6 --sysinfo >> hardware.txt
-cat hardware.txt
-
-ls
-pwd
+date > /root/hardware.txt
+./geekbench6 --sysinfo >> /root/hardware.txt
+cat /root/hardware.txt
+cd /root
+echo bye
