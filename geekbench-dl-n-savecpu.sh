7d2b91b7bd55e8f1a1e8b140577a444b64518ed1
Nick Foster <me@nickfost.com>
2024-02-19 18:02:35 -0600

    Update geekbench-dl-n-savecpu.sh

--- geekbench-dl-n-savecpu.sh
+++ geekbench-dl-n-savecpu.sh
@@ -5,12 +5,20 @@
 # Constants
 GBVER=Geekbench-6.2.2-Linux.tar.gz
 GBDIR=Geekbench-6.2.2-Linux
+FILE=/root/hardware.txt
 
 wget https://cdn.geekbench.com/$GBVER
 tar -zxf $GBVER
 cd $GBDIR
-date > /root/hardware.txt
-./geekbench6 --sysinfo >> /root/hardware.txt
+
+echo today is date > $FILE
+echo. >> $FILE
+./geekbench6 --sysinfo >> $FILE
+echo "---------------------------" >> $FILE
+echo "Run this mess yourself from: https://github.com/RandomDevelopment/geekbench-dl" >> $FILE
+echo "bye" >> $FILE
+
+
 cat /root/hardware.txt
 cd /root
 echo bye
